2b32ecbaa5998974eac0a61487c3012aadf1cc7c
jbi <biehler@adco-engineering-gw.com>
2017-05-07 13:32:41 +0200

    added RMSE computation to tools from previous implementation in extended kalman filter project

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,29 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  // make some sanity checks
+  assert(estimations.size() > 0);
+  assert(estimations.size() == ground_truth.size());
+
+  VectorXd rmse(4);
+  rmse << 0.0, 0.0, 0.0, 0.0;
+
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
+
+  	VectorXd residual = estimations[i] - ground_truth[i];
+
+  	//coefficient-wise multiplication
+  	residual = residual.array()*residual.array();
+  	rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
